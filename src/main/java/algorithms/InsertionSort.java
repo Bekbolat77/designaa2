0934d7e0e75d8fc5aab3e9a10474a1f31da1568b
Bekbolat77 <241631@astanait.edu.kz>
2025-09-29 22:52:46 +0500

    Update InsertionSort.java

--- src/main/java/algorithms/InsertionSort.java
+++ src/main/java/algorithms/InsertionSort.java
@@ -8,14 +8,14 @@ public class InsertionSort implements SortAlgorithm {
     public void sort(int[] a, PerformanceTracker t) {
         if (a == null) throw new IllegalArgumentException("array is null");
         int n = a.length;
-        t.alloc(0); // in-place
+        t.alloc(0); 
 
         for (int i = 1; i < n; i++) {
             int key = a[i];
             t.access();
             int left = 0, right = i - 1;
 
-            // Binary search to find insert position
+            
             while (left <= right) {
                 int mid = (left + right) >>> 1;
                 t.access(); t.compare();
